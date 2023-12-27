b0153adf487637e1a86ef73f45ba17561036408c
Ariel Fajar Herdanto <127375787+arielfajar@users.noreply.github.com>
2023-12-27 16:37:52 +0700

    Add files via upload

---
+++ Kelompok 3_AES.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad
+
+def procces_key(key) :
+    return key[:8]
+
+text = "Belajar AES"
+key = procces_key(b'sayaariel')
+
+BLOCK_SIZE = 8
+
+des = DES.new(key, DES.MODE_ECB)
+padded_text = pad(text.encode(), BLOCK_SIZE)
+encrypted_text = des.encrypt(padded_text)
+decrypted_text = des.decrypt(encrypted_text)
+
+print("Plain Teks : ", text)
+print("Hasil Enkripsi : ", encrypted_text)
+print("Hasil Dekripsi : ", decrypted_text.decode())
+
+
+# In[ ]:
+
+
+
+
